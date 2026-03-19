07c5781a55e357e81fbe5b36c20f7520cfbb55de
Rommel Azucena <6520241+jrcaz@users.noreply.github.com>
2026-03-19 17:11:15 +0800

    Update checksum value for v0.5.1 artifact

--- Casks/codefleet.rb
+++ Casks/codefleet.rb
@@ -1,6 +1,6 @@
 cask "codefleet" do
   version "0.3.2"
-  sha256 "a192afd573204f390e0a174ff4547eda4902aa0b13b37bb07cf6e32ffb7ca179"
+  sha256 "9571994c9d836e5603ef3a5b465796fde68190b0b5822fa841b9712aa0c6da59"
 
   url "https://hcqyrqyvykdqpaueghhd.supabase.co/storage/v1/object/public/applications/CodeFleet_0.5.1_aarch64.dmg"
   name "CodeFleet"
