c7c34ee851c5ab63a7c3ecf5a453eeb7bea89900
Rommel Azucena <6520241+jrcaz@users.noreply.github.com>
2026-03-24 00:58:32 +0800

    Fix the right version

--- Casks/codefleet.rb
+++ Casks/codefleet.rb
@@ -1,5 +1,5 @@
 cask "codefleet" do
-  version "0.3.2"
+  version "0.5.2"
   sha256 "d7a6a971804e7d7ad0374c4c97ca9e50d1f43ac99384d79785f0bf187b4a39ac"
 
   url "https://hcqyrqyvykdqpaueghhd.supabase.co/storage/v1/object/public/applications/CodeFleet_0.5.2_aarch64.dmg"
