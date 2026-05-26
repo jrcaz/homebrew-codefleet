097a64e15c8f12e87f546a9d826b62bd983a6898
Rommel Azucena <6520241+jrcaz@users.noreply.github.com>
2026-05-26 16:03:50 +0800

    Update CodeFleet to version 0.4.0

--- Casks/codefleet.rb
+++ Casks/codefleet.rb
@@ -1,8 +1,8 @@
 cask "codefleet" do
-  version "0.2.0"
-  sha256 "c4f7c9fc5d6ef7a2df6c9097aa5c65694bb679d0a6b8006cea01560686f55f01"
+  version "0.4.0"
+  sha256 "0cd77a6a42926684f5da85c59abd5b42e59b4fb2e0c74930b2aa392d97452986"
 
-  url "https://codefleet.app/api/download/brew/macos/0.2.0"
+  url "https://codefleet.app/api/download/brew/macos/0.4.0"
   name "CodeFleet"
   desc "AI-first development workspace for seamless collaboration with multiple AI coding assistants"
   homepage "https://codefleet.app"
